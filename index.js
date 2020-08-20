1a8bffa3a630f710ad9623ed5b6b520ad601792d
ToriK17 <vfcad@protonmail.com>
2020-08-19 18:29:20 -0600

    falls into the abyss

---
+++ index.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const width = (canvas.width = 320);
+const height = (canvas.height = 480);
+const boxwidth = 30;
+const boxheight = 60;
+
+// canvas centered in window
+canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
+
+// must name this box https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+const box = {
+    x: ((width / 2) - (boxwidth / 2)), 
+    y: ((height / 2) - (boxwidth /2 )),
+    w: boxwidth,
+    h: boxheight,
+    vel: { x: 0, y: 0},
+    color: "purple"
+};
+
+// context is like the 'pen' on the canvas.
+context.fillStyle = box.color;
+context.fillRect(box.x, box.y, box.w, box.h);
+
+// 'request animation frame' works to create a function that repeates itself over time. 
+function update() {
+    context.clearRect(0, 0, width, height);
+    box.y += 0.5;
+
+    context.fillStyle = box.color;
+    context.fillRect(box.x, box.y, box.w, box.h);
+
+    requestAnimationFrame(update);
+} 
+
+update();
