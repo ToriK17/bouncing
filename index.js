486e1d9857bfe302fafd559145b649dc86afcbe3
ToriK17 <vfcad@protonmail.com>
2020-08-19 18:53:54 -0600

    it's a box and it bounces with a left and a right arrow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const context = canvas.getContext("2d");
 const width = (canvas.width = 320);
 const height = (canvas.height = 480);
 const boxwidth = 30;
-const boxheight = 60;
+const boxheight = 40;
 const gravity = 0.1;
 
 // canvas centered in window
@@ -26,10 +26,15 @@ context.fillRect(box.x, box.y, box.w, box.h);
 // 'request animation frame' works to create a function that repeates itself over time. 
 function update() {
     context.clearRect(0, 0, width, height);
-    // velocity has magnitude and direction. 
+    // velocity has magnitude and direction. We always need this to update the x and y axis with velocity.
     box.vel.y += gravity;
     box.y += box.vel.y;
+    box.x += box.vel.x;
 
+    if (box.x + box.w > width || box.x < 0) {
+      box.vel.x *= -1;
+    }
+    
     if (box.y + box.h > height) {
       box.y = height - box.h;
       box.vel.y *= -1;
@@ -40,5 +45,13 @@ function update() {
 
     requestAnimationFrame(update);
 } 
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.keyCode === 37) {
+      box.vel.x = -1;
+  } else if (e.keyCode === 39) {
+      box.vel.x = 1;
+  }
+});
 
 update();
